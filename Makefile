3c97ec269b4a08fe9493532c4a011ed64884a300
zhengwenchao <zhengwenchao@baidu.com>
2016-12-22 11:59:30 +0800

    visualize PCD

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
 build:
-	g++ show_point_cloud.cpp -I/usr/include/pcl-1.7/ -I/usr/include/eigen3/ -lpcl_io -lboost_system -o show_point_cloud 
+	g++ show_point_cloud.cpp -I/usr/include/pcl-1.7/ -I/usr/include/eigen3/ -I/usr/include/vtk-5.8/ -lpcl_io -lpcl_common -lpcl_visualization -lboost_system -o show_point_cloud 
--- show_point_cloud.cpp
+++ show_point_cloud.cpp
@@ -7,28 +7,36 @@
 #include <iostream>
 #include <string>
 
-#include <pcl/io/file_io.h>
-namespace adu {
-namespace perception {
+#include <pcl/point_types.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/visualization/cloud_viewer.h>
+
+void viewOneOff(pcl::visualization::PCLVisualizer& viewer) {
+    viewer.setBackgroundColor(0, 0, 0); // set background black.
+}
 
 int main() {
+    //Read from File
     pcl::PCDReader file_reader;
     std::string file_name = "/home/erlangz/3D_point_cloud/0711/original_cloud/QB9178_12_1461753402_1461753702_3641.pcd";
-    pcl::PCLPointCloud2 point_cloud; //Point Cloud
+    pcl::PointCloud<pcl::PointXYZ>::Ptr point_cloud(new pcl::PointCloud<pcl::PointXYZ>); //Point Cloud MonochromeCloud(pcl::PointCloud<pcl::PointXYZ>)
     Eigen::Vector4f origin;          //sensor acquistion origin
     Eigen::Quaternionf orientation;  //the sensor acquistion orientation
     int file_version = 0;            //file version 
     int offset = 0;                  //[input] offset, you may change this when read from tar file.
 
-    int ret = file_reader.read(file_name, point_cloud, origin, orientation, file_version, offset);
+    int ret = file_reader.read(file_name, *point_cloud, offset);
     if (ret != 0) {
         std::cerr << "file:" << file_name << " open failed. ret:" << ret << std::endl;
         return -1;
     }
-    
     std::cout << "Open file:" << file_name << " file_version:" << file_version << std::endl;
 
+    //Show on the Screen
+    pcl::visualization::CloudViewer cloud_viewer("cloud_viewer"); 
+    cloud_viewer.showCloud(point_cloud);
+    cloud_viewer.runOnVisualizationThreadOnce(viewOneOff);
+
+    std::cin >> ret;
     return 0;
-}
-}
 }

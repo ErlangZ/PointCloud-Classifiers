27bb6ce8f11d72e629b119e91fec1d7ac52d71b8
zhengwenchao <zhengwenchao@baidu.com>
2016-12-29 17:31:41 +0800

    ADD: train_muliboost_model

--- train_multiboost_model.sh
+++ train_multiboost_model.sh
@@ -1,10 +1,28 @@
 #!/bin/bash
-MULTIBOOST_ROOT=/home/erlangz/MultiBoost/build/
-$MULTIBOOST_ROOT/multiboost \
-    --d "\t" \
-    --fileformat simple \
-    --verbose 3 \
-    --learnertype SingleStumpLearner \
-    --outputinfo result.data --shypname shyp.xml \
-    --traintest ../data/train/X-axis ../data/test/X-axis 10 \
-    --examplename
+train_model() {
+    TrainFile=$1
+    IterNumber=$2
+    ./Multiboost/build/multiboost \
+        --d "\t" \
+        --fileformat simple \
+        --examplename \
+        --verbose 3 \
+        --learnertype SingleStumpLearner \
+        --outputinfo ./model/result.data \
+        --shypname ./model/shyp.xml \
+        --train $TrainFile $IterNumber 
+}
+
+test_model() {
+    TestFile=$1
+    IterNumber=$2
+    ./Multiboost/build/multiboost \
+        --d "\t" \
+        --fileformat simple \
+        --examplename \
+        --verbose 3 \
+        --learnertype SingleStumpLearner \
+        --test $TestFile ./model/shyp.xml $IterNumber 
+}
+
+mkdir -p model
